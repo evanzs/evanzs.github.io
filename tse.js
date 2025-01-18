1b0859177a2b38f619dd7812db771cc99e1f98bc
Evandro Fernandes <evandrofidk@hotmail.com>
2025-01-18 16:56:38 -0300

    Add files via upload

---
+++ tse.js
@@ -0,0 +1,29 @@
+
+function splitTextContent(textContent) {
+    console.log(textContent)
+     const text = textContent.split('/');    
+    return text;
+}
+
+
+function parseTextToArray(obj) {    
+    return obj.map(line => {
+        const [ID, NOME, NOTA_OBJ] = line.split(',');
+        const TOTAL = parseFloat(NOTA_OBJ);
+        return { ID, NOME, NOTA_OBJ, TOTAL };
+    });
+}
+
+function parseTextToArrayTypeThree(obj) {    
+    return obj.map(line => {
+        const [ID, NOME, NOTA_OBJ,NOTA_DISC] = line.split(',');
+        const TOTAL = parseFloat(NOTA_OBJ) + parseFloat(NOTA_DISC);
+        return { ID, NOME, NOTA_OBJ,NOTA_DISC, TOTAL };
+    });
+}
+
+function sortArrayByProperty(array, column) {    
+        return array.sort((a, b) => b[column] - a[column]);
+}
+
+
